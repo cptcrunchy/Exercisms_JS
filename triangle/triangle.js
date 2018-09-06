2b742a7f7552d66e8b0ff9132d3eb519068986d0
cpt_Crunch <jasong84@gmail.com>
2018-09-06 00:03:01 -0500

    continuing to work on scalene triangles

--- triangle/triangle.js
+++ triangle/triangle.js
@@ -1,25 +1,22 @@
-function equalLength(el,i, arr){
-    return (arr[0] == el )
+function equalLength(el, i) {
+  return (el === i)
 }
 
-function kind(...sides){
-    const validation1 = sides.every(side => side > 0);//?
-    const sumOfSides = sides.reduce((a,b)=>a+b)//?
-    const angle1 = sides[0] + sides[1];//?
-    const angle2 = sides[1] + sides[2];//?
-    
-    if(validation1 && sides.every(equalLength)) return "equilateral"
-    if(validation1 && sides.some(equalLength)) return "isosceles"
-    if(validation1 && !sides.every(equalLength)) return "scalene"
+function kind(...sides) {
+  const validation1 = sides.every(side => side > 0) // ?
+  const sumOfSides = sides.reduce((a, b) => a + b) // ?
+  const angle1 = (sides[0] + sides[1]) // ?
+  const angle2 = (sides[1] + sides[2]) // ?
+
+  if (validation1 && sides.every(equalLength)) return 'equilateral'
+  if (validation1 && sides.some(equalLength)) return 'isosceles'
+  if (angle1 >= sumOfSides || angle2 >= sumOfSides || !validation1) {
+    throw new Error('invalid lengths')
+  }
+  
+  return 'scalene'
 }
 
-kind(3,4,5)//?
-// kind(3,4,4)//?
-// kind(4,3,4)//?
-// kind(4,4,3)//?
-// kind(10,10,2)//?
-// kind(3,4,5)//?
+kind(5,4,2) // ?
 
-
-
-module.exports = kind;
+module.exports = kind
